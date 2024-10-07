3327340c63ded6fd78c63ac7c58a7bc61029aff5
Bartosz <bartekaugustyn@proton.me>
2024-10-07 15:23:53 +0200

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 "aha"
 "dziala?"
 "jak dodałeś z konsoli to tak xd"
+
+"===================="
+
+import pandas as pd
+
+df = pd.read_csv('../z_geoportal_up42.csv', encoding='utf-8')
+print(df.head())
